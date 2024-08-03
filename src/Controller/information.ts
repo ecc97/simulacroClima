4af8cee3e77e327ced92d8b679aec3896e63e3fd
ecc97 <carmonaedwin1123@gmail.com>
2024-08-03 12:16:20 -0500

    feat: se agrega ver más

--- src/Controller/information.ts
+++ src/Controller/information.ts
@@ -1,17 +1,20 @@
 import { ICity } from "../Model/ICitys"
+import '../scss/information.scss'
 
 const main = document.querySelector('#home-main') as HTMLElement
 const idCity = localStorage.getItem('id-view')
 
 async function getInfo(): Promise<ICity>{
     const response = await fetch(`http://localhost:3000/cities/${idCity}`)
-    const data = await response.json()
+    const data: ICity = await response.json()
     console.log(data)
     return data
 }
 
 function showInfo(props: ICity){
     const {city, country, image, cityDescription} = props;
+    const container = document.createElement('div') as HTMLElement
+    container.className = 'card-container'
 
     const img = document.createElement('img') as HTMLImageElement
     img.className = 'card-image'
@@ -27,16 +30,25 @@ function showInfo(props: ICity){
     const description = document.createElement("p") as HTMLParagraphElement
     description.innerText = cityDescription
 
-    main.append(img, title, cardCountry, description)
+    container.append(img, title, cardCountry, description)
+    main.append(container)
+
+    return main
 }
 
-const informationCity = getInfo()
 
-// async function renderAll() {
-//    showInfo(informationCity)
-// }
+async function renderAll() {
+    try {
+        const informationCity: ICity = await getInfo()
+        showInfo(informationCity)
+    } catch (error) {
+        console.error(error)
+        alert('Error al cargar la información')
+        main.innerHTML = 'Error al cargar la información'
+    }
+}
 
-// renderAll()
+renderAll()
 
 
 
